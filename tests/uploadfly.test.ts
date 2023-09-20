12c2cb788031016c91e29f33bc7d1d2c6959c656
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-09-20 01:48:12 +0100

    Finished writing integration tests

---
+++ tests/uploadfly.test.ts
@@ -0,0 +1,33 @@
+import UploadflyClient from "../src/index";
+
+describe("UploadflyClient", () => {
+  const client = new UploadflyClient("uf_99d846ef15c041a8bf6dd1bd4200cc8b");
+
+  it("should upload a file and return valid response", async () => {
+    const blob = new Blob(["file-content"], { type: "text/plain" });
+    const file = new File([blob], "testing.txt", {
+      lastModified: Date.now(),
+    });
+
+    const response = await client.uploadFile(file);
+
+    expect(response.success).toBe(true);
+    expect(response.status).toBe(201);
+    expect(response.data).toMatchObject({
+      name: expect.anything(),
+      data: expect.anything(),
+      size: expect.anything(),
+      type: expect.anything(),
+      url: expect.anything(),
+    });
+  });
+
+  it("should delete a file and return valid response", async () => {
+    const testFile = "https://cdn.uploadfly.cloud/9fpKZy/WhiteSur.jpg";
+    const response = await client.deleteFile(testFile);
+
+    expect(response.success).toBe(true);
+    expect(response.status).toBe(200);
+    expect(response.data.message).toContain("deleted successfully");
+  });
+});

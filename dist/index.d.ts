f82679c219126f3c49b2494e8ae9b25a3d7c30b2
akinloluwami <akinkunmioye42@gmail.com>
2023-11-18 05:31:31 +0100

    deleted stuff

--- dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-import UploadflyClient from "./uploadfly";
-export default UploadflyClient;
--- tests/uploadfly.test.ts
+++ tests/uploadfly.test.ts
@@ -1,7 +1,11 @@
 import UploadflyClient from "../src/index";
 
+import dotenv from "dotenv";
+dotenv.config();
+
 describe("React SDK", () => {
-  const client = new UploadflyClient("uf_99d846ef15c041a8bf6dd1bd4200cc8b");
+  const apiKey = process.env.REACT_APP_UPLOADFLY_API_KEY;
+  const client = new UploadflyClient(apiKey!);
   let uploadedFileUrl: string;
 
   it("should upload a PNG file and return valid response", async () => {
@@ -19,8 +23,6 @@ describe("React SDK", () => {
 
     const response = await client.uploadFile(file);
 
-    console.log(response);
-
     expect(response.success).toBe(true);
     expect(response.status).toBe(201);
 
@@ -31,8 +33,6 @@ describe("React SDK", () => {
   it("should delete the uploaded file and return valid response", async () => {
     // Use the captured URL of the uploaded file
     const response = await client.deleteFile(uploadedFileUrl);
-
-    console.log(response);
 
     expect(response.success).toBe(true);
     expect(response.status).toBe(200);

1039f312d2eb81a94356e936a865617302fd92ab
akinloluwami <akinkunmioye42@gmail.com>
2023-12-10 06:01:00 +0100

    >>>

--- src/uploadfly.ts
+++ src/uploadfly.ts
@@ -1,7 +1,6 @@
 export class CreateUploadflyClient {
   private apiKey: string;
-  private uploadflyUploadEndpoint: string = `https://api.uploadfly.cloud/upload`;
-  private uploadflyDeleteEndpoint: string = `https://api.uploadfly.cloud/delete`;
+  private uploadflyEndpoint: string = `https://api.uploadfly.cloud`;
 
   constructor(apiKey: string) {
     if (!apiKey) throw new Error("An API key is required.");
@@ -20,7 +19,7 @@ export class CreateUploadflyClient {
       formData.append("file", file);
       formData.append("filename", config?.filename || file.name);
 
-      const response = await fetch(this.uploadflyUploadEndpoint, {
+      const response = await fetch(`${this.uploadflyEndpoint}/upload`, {
         method: "POST",
         body: formData,
         headers: {
@@ -37,7 +36,7 @@ export class CreateUploadflyClient {
   public async delete(fileUrl: string) {
     if (!fileUrl) throw new Error("A file url must be provided.");
     try {
-      const response = await fetch(this.uploadflyDeleteEndpoint, {
+      const response = await fetch(`${this.uploadflyEndpoint}/delete`, {
         method: "DELETE",
         headers: {
           Authorization: `Bearer ${this.apiKey}`,
@@ -53,4 +52,40 @@ export class CreateUploadflyClient {
       );
     }
   }
+
+  public image = {
+    upload: async (
+      file: File,
+      config?: {
+        filename?: string;
+        maxFileSize?: string;
+        width?: number;
+        height?: number;
+      }
+    ) => {
+      if (!file) throw new Error("A file is required.");
+      try {
+        const formData = new FormData();
+        formData.append("file", file);
+        formData.append("filename", config?.filename || file.name);
+        formData.append("maxFileSize", config?.maxFileSize || "");
+        formData.append("width", config?.width?.toString() || "");
+        formData.append("height", config?.height?.toString() || "");
+
+        const response = await fetch(`${this.uploadflyEndpoint}/image/upload`, {
+          method: "POST",
+          body: formData,
+          headers: {
+            Authorization: `Bearer ${this.apiKey}`,
+          },
+        });
+        if (!response.ok) throw new Error("Image upload failed.");
+        return response.json();
+      } catch (error: unknown | any) {
+        throw new Error(
+          `An error occurred during image upload: ${error.message}`
+        );
+      }
+    },
+  };
 }
